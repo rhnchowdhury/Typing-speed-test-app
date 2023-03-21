5202e6d8db443c92271923bb399d20cae421a7c9
Raihan <rhnchdri@gmail.com>
2023-03-21 08:53:25 +0600

    typing test all done

--- src/Components/Typing.js
+++ src/Components/Typing.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import randomWords from 'random-words';
 
 const numOfWords = 200;
@@ -9,26 +9,48 @@ const Typing = () => {
     const [countDown, setCountDown] = useState(seconds);
     const [currentInput, setCurrentInput] = useState('');
     const [currWordIdx, setCurrWordIdx] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const [error, setError] = useState(0);
+    const [status, setStatus] = useState('waiting');
+    const textInput = useRef(null);
 
     useEffect(() => {
         setWords(generateWords())
     }, []);
+
+    useEffect(() => {
+        if (status === 'started') {
+            textInput.current.focus()
+        }
+    }, [status])
 
     function generateWords() {
         return new Array(numOfWords).fill(null).map(() => randomWords())
     };
 
     const timeStart = () => {
-        let interval = setInterval(() => {
-            setCountDown((preCountDown) => {
-                if (preCountDown === 0) {
-                    clearInterval(interval);
-                }
-                else {
-                    return preCountDown - 1;
-                }
-            })
-        }, 1000)
+        if (status === 'finished') {
+            setWords(generateWords())
+            setCurrWordIdx(0);
+            setCorrect(0);
+            setError(0);
+        }
+        if (status !== 'started') {
+            setStatus('started')
+            let interval = setInterval(() => {
+                setCountDown((preCountDown) => {
+                    if (preCountDown === 0) {
+                        clearInterval(interval);
+                        setStatus('finished');
+                        setCurrentInput('');
+                        return seconds;
+                    }
+                    else {
+                        return preCountDown - 1;
+                    }
+                })
+            }, 1000)
+        }
     };
 
     const handleInput = ({ keyCode }) => {
@@ -42,7 +64,12 @@ const Typing = () => {
     const checkMatch = () => {
         const wordToCompare = words[currWordIdx];
         const itMatch = wordToCompare === currentInput.trim();
-        console.log({ itMatch })
+        if (itMatch) {
+            setCorrect(correct + 1);
+        }
+        else {
+            setError(error + 1)
+        }
     }
 
     return (
@@ -51,24 +78,33 @@ const Typing = () => {
                 <h1>Check your typing skills in a minute</h1>
                 <h1>{countDown}</h1>
                 <div>
-                    <input type="text" onKeyDown={handleInput} value={currentInput} onChange={(e) => setCurrentInput(e.target.value)} className='input' />
+                    <input ref={textInput} disabled={status !== 'started'} type="text" onKeyDown={handleInput} value={currentInput} onChange={(e) => setCurrentInput(e.target.value)} className='input' />
                 </div>
                 <div>
                     <button onClick={timeStart}>Start Test</button>
                 </div>
-                <div>
-                    {words.map((word, i) => (
-                        <span key={i}>
-                            <span>
-                                {word.split("").map((char, idx) => (
-                                    <span key={idx}>{char}</span>
-                                ))}
+                {status === 'started' && (
+                    <div>
+                        {words.map((word, i) => (
+                            <span key={i}>
+                                <span>
+                                    {word.split("").map((char, idx) => (
+                                        <span key={idx}>{char}</span>
+                                    ))}
+                                </span>
+                                <span> </span>
                             </span>
-                            <span> </span>
-                        </span>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
+
             </div>
+            {status === 'finished' && (
+                <div>
+                    <h1>Words Per Minute:{correct}</h1>
+                    <h1>Accuracy: {Math.round((correct / (correct + error)) * 100)}%</h1>
+                </div>
+            )}
         </section>
     );
 };
